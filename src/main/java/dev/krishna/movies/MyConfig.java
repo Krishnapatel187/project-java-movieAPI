2a0c53347cb7786c3630136c274f5b6b25d8695e
Krishna Patel <kcpn14@gmail.com>
2023-05-31 13:25:26 -0400

    fibnal commit

---
+++ src/main/java/dev/krishna/movies/MyConfig.java
@@ -0,0 +1,17 @@
+package dev.krishna.movies;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyConfig  implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(false).maxAge(3600);
+    }
+}
